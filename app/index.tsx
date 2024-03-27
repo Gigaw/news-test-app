6d4d06895e96f284cd744c41e5d40556f6e31968
Gigolaev Igor <41339681+Gigaw@users.noreply.github.com>
2024-03-27 15:01:37 +0400

    infinite scroll

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,39 @@
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Button,
+  RefreshControl,
+} from "react-native";
+import React from "react";
 import Spacer from "@/components/spacer";
 import NewsItem from "@/components/news-item";
-import { getNews } from "@/api/news-api";
-import useSWR from "swr";
-import dayjs from "dayjs";
 import AppText from "@/components/app-text";
+import useGetNews from "@/hooks/useGetNews";
 
 export default function App() {
-  const [pageIndex, setPageIndex] = useState(1);
-  const now = dayjs();
-  const weekAgo = now.subtract(7, "day");
-  const { data, error, isLoading } = useSWR(`/api/news?page=${pageIndex}`, () =>
-    getNews({ from: weekAgo.format("YYYY-MM-DD") })
-  );
-  const filteredData = data?.filter(
-    (item) => item.title !== "[Removed]" && item.description !== "[Removed]"
-  );
+  const {
+    data: filteredData,
+    error,
+    isLoading,
+    isLoadingMore,
+    isReachingEnd,
+    isRefreshing,
+    size,
+    setSize,
+  } = useGetNews();
+
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.container}>
         {!!error && <AppText>Something went wront try later</AppText>}
-        {isLoading && <Text>Loading...</Text>}
+        {isLoading && !isLoadingMore && <Text>Loading...</Text>}
         {!isLoading && !error ? (
           <FlatList
+            refreshControl={<RefreshControl refreshing={!!isRefreshing} />}
+            refreshing={isRefreshing}
             data={filteredData}
             ListEmptyComponent={() => <AppText>No Data</AppText>}
             style={styles.list}
@@ -31,6 +41,20 @@ export default function App() {
             renderItem={({ item }) => <NewsItem item={item} />}
             ItemSeparatorComponent={() => Spacer({ height: 20 })}
             keyExtractor={(item, index) => index.toString()}
+            ListFooterComponent={() => (
+              <>
+                <Spacer height={20} />
+                {isLoadingMore && (
+                  <AppText textAlign="center">Loading More...</AppText>
+                )}
+                {isReachingEnd && (
+                  <AppText textAlign="center">Reached End</AppText>
+                )}
+                {!isReachingEnd && !isLoadingMore ? (
+                  <Button title="Load More" onPress={() => setSize(size + 1)} />
+                ) : null}
+              </>
+            )}
           />
         ) : null}
       </View>
---
+++ hooks/useGetNews.ts
@@ -0,0 +1,47 @@
+import { newsApi } from "@/api/news-api";
+import dayjs from "dayjs";
+import useSWRInfinite from "swr/infinite";
+import { NewsResponse } from "@/types/news";
+
+const PAGE_SIZE = 10;
+
+export default function useGetNews() {
+  const now = dayjs();
+  const weekAgo = now.subtract(7, "day");
+  const { data, error, size, setSize, isValidating, isLoading } =
+    useSWRInfinite(
+      (index) =>
+        `/everything?from=${weekAgo.format(
+          "YYYY-MM-DD"
+        )}sortBy=popularity&q=bitcoin&pageSize=${PAGE_SIZE}&page=${
+          index + 1
+        }&apiKey=${process.env.EXPO_PUBLIC_NEWS_API_KEY}`,
+      (url) =>
+        newsApi.get<NewsResponse>(url).then((res) => {
+          return res.data.articles;
+        })
+    );
+
+  const isLoadingMore =
+    isLoading || (size > 0 && data && typeof data[size - 1] === "undefined");
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
+  const isRefreshing = isValidating && data && data.length === size;
+
+  const flatData = data?.flat();
+  const filteredData = flatData?.filter(
+    (item) => item.title !== "[Removed]" && item.description !== "[Removed]"
+  );
+
+  return {
+    data: filteredData,
+    error,
+    isLoading,
+    isLoadingMore,
+    isReachingEnd,
+    isRefreshing,
+    size,
+    setSize,
+  };
+}
